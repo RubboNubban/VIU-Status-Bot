276212cdc232a55fcfc6956239337f33777dc3ce
Ronan Leighton <samuraironan@gmail.com>
2020-01-14 16:54:53 -0800

    Changed emote options

--- yeet.js
+++ yeet.js
@@ -1,12 +1,18 @@
+//includes
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const http = require('http');
-const Enmap = require("enmap");
-const enmap = new Enmap({ name: "VIU_Status" });
 
+//site
+const gateway = 'http://viu.apparmor.com/Tools/AlertHistory/';
+
+var newestAlert;
+var alerts = [];
+
+//Update alerts[]
 function getData() {
-  http.get('http://viu.apparmor.com/Tools/AlertHistory/', (res) => {
+  http.get(gateway, (res) => {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
 
@@ -29,24 +35,8 @@ function getData() {
     res.on('end', () => {
       try {
         const parsedData = JSON.parse(rawData);
-        const dataMsg = `**${parsedData.AlertFeedItems[0].DateTimeString}**\n\
-**${parsedData.AlertFeedItems[0].Title}**\n\n\
-${parsedData.AlertFeedItems[0].Description}`;
-        //console.log(parsedData);
-        if (!enmap.has('statuses')) {
-          enmap.set('statuses', [dataMsg]);
-        } else {
-          if (enmap.get('statuses').indexOf(dataMsg) == -1) {
-            enmap.push('statuses', dataMsg);
-            client.channels.get('467017233307009045').send(dataMsg);
-          } else {
-            return;
-          }
-          if (enmap.get('statuses').length > 3) {
-            let array = enmap.get('statuses');
-            enmap.remove('statuses', array[0]);
-          }
-        }
+        alerts = parsedData.AlertFeedItems;
+        updateLatestAlert();
       } catch (e) {
         console.error(e.message);
       }
@@ -56,11 +46,47 @@ ${parsedData.AlertFeedItems[0].Description}`;
   });
 };
 
+//Converts an alert into a string that's ready to send in discord
+function alertToString(a)
+{
+  return `**${a.DateTimeString}**\n\
+**${a.Title}**\n\n\
+${a.Description}`;
+}
+
+var autoChannels = []
+function updateLatestAlert()
+{
+  if(alerts[0] != newestAlert)  {
+    newestAlert = alerts[0];
+    for(let i = 0 ; i < autoChannels; i++)
+      autoChannels.send(alertToString(newestAlert));
+  }
+}
+function autoAnnouncement()
+{
+  getData();
+  updateLatestAlert();
+}
+
+var globalTimer;
 client.on('ready', () => {
   console.log('Online and running');
   getData();
-  setInterval(getData, 600000);
+  globalTimer = setInterval(autoAnnouncement, 600000);
 });
+
+//code stolen from https://stackoverflow.com/questions/3954438/how-to-remove-item-from-array-by-value
+Array.prototype.remove = function() {
+  var what, a = arguments, L = a.length, ax;
+  while (L && this.length) {
+      what = a[--L];
+      while ((ax = this.indexOf(what)) !== -1) {
+          this.splice(ax, 1);
+      }
+  }
+  return this;
+};
 
 let prefix = config.prefix;
 client.on("message", (message) => {
@@ -71,74 +97,75 @@ client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
     switch (command) {
       case 'alert':
-        http.get('http://viu.apparmor.com/Tools/AlertHistory/', (res) => {
-          const { statusCode } = res;
-          const contentType = res.headers['content-type'];
+        message.channel.send(alertToString(newestAlert)).then(sent => {
 
-          let error;
-          if (statusCode !== 200) {
-            error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-          } else if (!/^text\/html/.test(contentType)) {
-            error = new Error('Invalid content-type.\n' + `Expected text/html but received ${contentType}`);
+          const reacts = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟'];
+          var currentPage = 0;
+          let max = alerts.length - 1;
+
+          function addButtons() {
+            for (let i = 0; i <= max; i++) {
+              setTimeout(function() {
+                sent.react(reacts[i]);
+              }, i * 300);
+            }
           }
-          if (error) {
-            console.error(error.message);
-            // Consume response data to free up memory
-            res.resume();
+
+          function createCollecterMessage(msg) {
+              const filter = (reaction, user) => {
+                return reacts.indexOf(reaction.emoji.name) !== -1 && user.id === author;
+              };
+              const collector = msg.createReactionCollector(filter, { time: 120000 });
+
+              collector.on('collect', (reaction, reactionCollector) => {
+                msg.edit(alertToString(alerts[reacts.indexOf(reaction.emoji.name)]));
+                reaction.remove(author);
+              });
+          }
+
+          addButtons();
+          createCollecterMessage(sent);
+
+        });
+      break;
+      case 'subscribe':
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+          message.channel.send("You don't have permissions to subscribe to a channel!");
+          return;
+        }
+        try {
+          var channel = client.channels.find('name', args[0]);
+          if (!channel) {
+            return message.channel.send("Could not find channel: " + args[0]);
+          } else if (autoChannels.indexOf(channel.id) !== -1) {
+            return message.channel.send("Already subscribed to channel: " + args[0]);
+          }
+          autoChannels.push(channel.id);
+          message.channel.send("Subscribed to " + args[0]);
+        }
+        catch (e) {
+          console.error(e);
+        }
+      break;
+      case 'unsubscribe':
+          if(!message.member.hasPermission("ADMINISTRATOR")) {
+            message.channel.send("You don't have permissions to unsubscribe from a channel!");
             return;
           }
-
-          res.setEncoding('utf8');
-          let rawData = '';
-          res.on('data', (chunk) => { rawData += chunk; });
-          res.on('end', () => {
-            try {
-              const parsedData = JSON.parse(rawData);
-              const dataMsg = `**${parsedData.AlertFeedItems[0].DateTimeString}**\n\
-**${parsedData.AlertFeedItems[0].Title}**\n\n\
-${parsedData.AlertFeedItems[0].Description}`;
-              //console.log(parsedData);
-              if (enmap.get('statuses').indexOf(dataMsg) == -1) {
-                enmap.push('statuses', dataMsg);
-              }
-              for (let i = 3; i < enmap.get('statuses').length; i++) {
-                let array = enmap.get('statuses');
-                enmap.remove('statuses', array[0]);
-              }
-              console.log(enmap.get('statuses'));
-              message.channel.send(dataMsg).then(sent => {
-                const statusArray = enmap.get('statuses');
-                var currentPage = statusArray.length - 1;
-                console.log(`Statuses: ${statusArray.length}\n${statusArray}`);
-                function createCollecterMessage(msg) {
-                  const filter = (reaction, user) => {
-                    return reaction.emoji.name === '⬅' || reaction.emoji.name === '➡' && user.id === author;
-                  };
-                  const collector = msg.createReactionCollector(filter, { time: 120000 });
-
-                  collector.on('collect', (reaction, reactionCollector) => {
-                    if (reaction.emoji.name === '⬅' && currentPage !== 0) {
-                      msg.edit(statusArray[currentPage--]);
-                    } else if (reaction.emoji.name === '➡' && currentPage !== (statusArray.length - 1)) {
-                      msg.edit(statusArray[currentPage++]);
-                    }
-                    console.log('Page: ', currentPage);
-                    reaction.remove(author);
-                  });
-                }
-
-                sent.react('⬅')
-                .then(sentReaction => sentReaction.message.react('➡'))
-                .then(sentReaction => createCollecterMessage(sentReaction.message));
-              });
-            } catch (e) {
-              console.error(e.message);
+          try {
+            var channel = client.channels.find('name', args[0]);
+            if (!channel) {
+              return message.channel.send("Could not find channel: " + args[0]);
+            } else if (autoChannels.indexOf(channel.id) === -1) {
+              return message.channel.send("Not subscribed to channel: " + args[0]);
             }
-          });
-        }).on('error', (e) => {
-          console.error(`Got error: ${e.message}`);
-        });
-        break;
+            autoChannels.remove(channel.id);
+            message.channel.send("Unsubscribed from " + args[0]);
+          }
+          catch (e) {
+            console.error(e);
+          }
+      break;
     }
 });
 
